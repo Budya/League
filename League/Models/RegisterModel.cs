4c9c5d218ca32a0de0d14f759911627832c1889e
budinskiy_d <budinskiy_d@BUDINSKIY>
2018-08-15 17:05:41 +0300

    FirstCommit

---
+++ League/Models/RegisterModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace League.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "Логин")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Электронная почта")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Повторите пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ OverrideRoleProvider/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OverrideRoleProvider.MvcApplication" Language="C#" %>
